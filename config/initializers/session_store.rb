827fd36c82bd2d69d09e38cf8e82b366d2f77266
Matthew Powers <matthewkevinpowers@gmail.com>
2012-12-09 17:46:42 -0800

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MiniYelp::Application.config.session_store :cookie_store, key: '_mini_yelp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MiniYelp::Application.config.session_store :active_record_store
