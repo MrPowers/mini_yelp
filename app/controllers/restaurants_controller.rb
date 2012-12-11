d1d78da707a6505252687f7a153e0612f60ebbc6
Matthew Powers <matthewkevinpowers@gmail.com>
2012-12-10 16:01:22 -0800

    finished updating reviews. finished cancan

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,10 @@
 class RestaurantsController < ApplicationController
+	load_and_authorize_resource
+
 	def new
-		@restaurant = Restaurant.new
 	end
 
 	def create
-		@restaurant = Restaurant.create(params[:restaurant])
 		@restaurant.user_id = current_user.id
 		if @restaurant.save
 			redirect_to restaurants_path
@@ -14,16 +14,12 @@ class RestaurantsController < ApplicationController
 	end
 
 	def index
-		@restaurants = Restaurant.all
 	end
 
 	def edit
-		@restaurant = Restaurant.find(params[:id])
 	end
 
 	def update
-		@restaurant = Restaurant.find(params[:id])
-		@restaurant.update_attributes(params[:restaurant])
 		if @restaurant.save
 			redirect_to restaurants_path
 		else
@@ -32,7 +28,6 @@ class RestaurantsController < ApplicationController
 	end
 
 	def destroy
-		@restaurant = Restaurant.find(params[:id])
 		@restaurant.destroy
 		redirect_to restaurants_path
 	end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,28 @@
 class ReviewsController < ApplicationController
+	load_and_authorize_resource :restaurant
+  load_and_authorize_resource :review, :through => :restaurant
+
 	def create
-		@restaurant = Restaurant.find(params[:restaurant_id])
 		@review = @restaurant.reviews.create(params[:review])
 		@review.user_id = current_user.id
-		@review.save
+		if @review.save
+			redirect_to @restaurant
+		else
+			render "reviews/form"
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		@review.update_attributes(params[:review])
+		@review.user_id = current_user.id
 		redirect_to @restaurant
+	end
+
+	def destroy
+		@review.destroy
+		redirect_to @restaurant		
 	end
 end
